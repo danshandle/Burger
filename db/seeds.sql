e6d0b1f8aeb7594918231849180f852503c891a5
Air Dan <danshandle@gmail.com>
2020-01-16 20:28:41 -0600

    Project files completed

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Ham');
+INSERT INTO burgers (burger_name) VALUES ('Cat');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Dirt', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Beef', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Grass', true);
+INSERT INTO burgers (burger_name) VALUES ('Weed');
