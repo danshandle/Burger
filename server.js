723cf9e24e9d1b01bb1254e0683df6d653183c55
Air Dan <danshandle@gmail.com>
2020-01-16 21:15:27 -0600

    DB for Heroku fix

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var methodOverride = require("method-override");
 var bodyParser = require("body-parser");
 
-var port = 3000;
+var port = process.env.PORT || 3000;
 
 var app = express();
 
@@ -23,3 +23,4 @@ var routes = require("./controllers/burgers_controller.js");
 app.use("/", routes);
 
 app.listen(process.env.PORT || port);
+
