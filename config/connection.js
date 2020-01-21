c725a6ed6212aa22abefd19e05eb5cd012c1be8e
Air Dan <danshandle@gmail.com>
2020-01-21 16:59:43 -0600

    added jaws, added jaws to mysql workbench

--- config/connection.js
+++ config/connection.js
@@ -19,26 +19,4 @@ if (process.env.JAWSDB_URL){
 };
 
 
-
-
-
-
-// var connection = mysql.createConnection({
-//   port: 3306,
-//   host: "localhost",
-//   user: "root",
-//   password: "Qwert411!",
-//   database: "burgers_db"
-// });
-
-// Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
 module.exports = connection;
